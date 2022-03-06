e7eb62f7b049f7bb30b5b32a3b4fd046039b1208
Luiz Otávio Olinger Momm <luizotaviom@gmail.com>
2022-03-06 20:06:17 -0300

    Ajustes no projeto para MySql e outras entidades

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using Pomelo.EntityFrameworkCore;
 
 namespace SalesWebMvc
 {
@@ -26,9 +27,17 @@ namespace SalesWebMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
+            /*
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"),
+                    builder => builder.MigrationsAssembly("SalesWebMvc")));
+            */
+            services.AddDbContext<SalesWebMvcContext>(options =>
+            {
+                var connetionString = Configuration.GetConnectionString("ConnectionStrings");
+                connetionString = "server=localhost;userid=developer;password=123456;database=saleswebmvcappdb";
+                options.UseMySql(connetionString, ServerVersion.AutoDetect(connetionString));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

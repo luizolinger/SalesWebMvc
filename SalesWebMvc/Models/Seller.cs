6ea020b5d34fd200e2065e2d9e67509657e74a37
Luiz Otávio Olinger Momm <luizotaviom@gmail.com>
2022-05-04 20:13:11 -0300

    Foreign key not null

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -11,7 +11,8 @@ namespace SalesWebMvc.Models
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
-        public Department Departament { get; set; }
+        public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
@@ -25,7 +26,7 @@ namespace SalesWebMvc.Models
             Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
-            Departament = departament;
+            Department = departament;
         }
 
         public void AddSales(SalesRecord sr)
